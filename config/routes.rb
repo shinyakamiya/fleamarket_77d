c510938fc761817cc69e0d2601479c6c32c1f53a
daisuke19990614 <m.daisuke0614@gmail.com>
2020-06-22 16:30:48 +0900

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'items/index'
+  get 'items/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'items#index'
+  root 'items#show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   

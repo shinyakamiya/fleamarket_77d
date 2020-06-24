a7aad628ac307a49c31db3e9eb4c8f64bcccf8bb
daisuke19990614 <m.daisuke0614@gmail.com>
2020-06-24 17:57:48 +0900

    購入確認ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'items/index'
+  get 'items/show'
+  resources :products, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'items#index'
+  root 'items#show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
 end

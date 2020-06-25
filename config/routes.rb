162ea680bd49a7f729888fdff719bd23727d9b20
shinyakamiya <shinya.600708.kouichi@gmail.com>
2020-06-25 14:19:38 +0900

    マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :credit_cards
+  resources :users 
+  resources :cards
+    
+  # root to: 'mypages#index'
   
 end
